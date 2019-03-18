e5383e5f80e8bc99f8c85af0a641d6cb1143312c
nick <iiasceri@student.42.fr>
2019-03-18 16:13:25 +0200

    Game Starts!

--- src/scene.py
+++ src/scene.py
@@ -1,13 +1,21 @@
-from PyQt5.QtWidgets import QGraphicsScene
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsSceneMouseEvent
 
 
 class Scene(QGraphicsScene):
 
     def __init__(self, parent):
         QGraphicsScene.__init__(self)
+        self.i = 0
+        def sceneEventFilter(o, event):
+            # print(o, event)
+            # print(type(event))
 
-        def sceneEventFilter(o, e):
-            print(o, e)
+
+            if isinstance(event, QGraphicsSceneMouseEvent):
+                print("Ai umblat pe scena", self.i)
+                self.i += 1
+            # print("QPoint({0}, {1}) in screen coords".format(
+            #     event.pos().x(), event.pos().y()))
             return False
         self.scene = QGraphicsScene(parent)
 
