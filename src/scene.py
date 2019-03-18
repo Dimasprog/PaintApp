869c321151a269d5d5ae14ed0ff2fab9265bc6f8
nick <iiasceri@student.42.fr>
2019-03-18 18:04:39 +0200

    Read The Mice Coords

--- src/scene.py
+++ src/scene.py
@@ -1,26 +1,17 @@
-from PyQt5.QtWidgets import QGraphicsScene, QGraphicsSceneMouseEvent
+from PyQt5.QtGui import QCursor
+from PyQt5.QtWidgets import QGraphicsScene
 
 
 class Scene(QGraphicsScene):
 
     def __init__(self, parent):
         QGraphicsScene.__init__(self)
-        self.i = 0
-        def sceneEventFilter(o, event):
-            # print(o, event)
-            # print(type(event))
-
-
-            if isinstance(event, QGraphicsSceneMouseEvent):
-                print("Ai umblat pe scena", self.i)
-                self.i += 1
-            # print("QPoint({0}, {1}) in screen coords".format(
-            #     event.pos().x(), event.pos().y()))
-            return False
         self.scene = QGraphicsScene(parent)
-
-        self.scene.installEventFilter(self.scene)
-        self.scene.eventFilter = sceneEventFilter
+        self.scene.mousePressEvent = self.mousePressEvent
 
     def getScene(self):
-        return self.scene;
+        return self.scene
+
+    def mousePressEvent(self, e):
+        p = QCursor.pos()
+        print(p.x())
