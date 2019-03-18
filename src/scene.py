12cede4b484f7f37c2ef6ddd95c8d86b6c36b83e
nick <iiasceri@student.42.fr>
2019-03-18 20:41:58 +0200

    Implemented first, second click for segments

--- src/scene.py
+++ src/scene.py
@@ -1,5 +1,6 @@
+from PyQt5.QtCore import QLineF
 from PyQt5.QtGui import QCursor
-from PyQt5.QtWidgets import QGraphicsScene
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsLineItem, QGraphicsItem
 
 
 class Scene(QGraphicsScene):
@@ -8,10 +9,29 @@ class Scene(QGraphicsScene):
         QGraphicsScene.__init__(self)
         self.scene = QGraphicsScene(parent)
         self.scene.mousePressEvent = self.mousePressEvent
+        self.firstClick = True
+        self.secondClick = False
+        self.firstClickPos = QCursor.pos()
+        self.secondClickPos = QCursor.pos()
 
     def getScene(self):
         return self.scene
 
     def mousePressEvent(self, e):
-        p = QCursor.pos()
-        print(p.x())
+        if self.firstClick:
+            self.firstClickPos = QCursor.pos()
+            self.firstClick = False
+        else:
+            self.secondClickPos = QCursor.pos()
+            self.secondClick = True
+
+        if self.secondClick:
+            print(self.firstClickPos, "Prime Click")
+            print(self.secondClickPos, "Second Click")
+
+
+            # TODO: Add those
+            # line = QLineF(10, 10, self.firstClickPos, self.secondClickPos)
+            # line_item = QGraphicsLineItem(line)
+            # line_item.setFlag(QGraphicsItem.ItemIsMovable, True)
+            # self.scene.addItem(line_item)
